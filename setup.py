acf4f09e57dd0f7022ea6bfa585c288f9ee7f116
james yang <yanghongday369@gmail.com>
2020-12-19 15:07:37 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def dirac_test_suite():
 
 setup(
     name='diracnet',
-    version='0.0.1',
+    version='0.0.2',
     description='',
     url='https://github.com/llxlr/diracnet',
     author='llxlr',
